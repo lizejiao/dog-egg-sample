c44d92f08fa2e88d24aa7d420746e20a5ab038b4
李苟蛋 <8346132@qq.com>
2022-07-05 11:31:18 +0800

    初始化 mybatis-plus 项目

---
+++ lgd-mybatis-plus/src/main/java/dog/egg/li/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,15 @@
+package dog.egg.li.mybatisplus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("dog.egg.li.mybatisplus")
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+}
