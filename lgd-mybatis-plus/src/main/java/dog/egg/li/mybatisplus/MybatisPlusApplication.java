3032aa4ecc2b5b57e8da78e9c2a56879ec5a7e6d
李苟蛋 <8346132@qq.com>
2022-07-06 11:07:23 +0800

    引入包

--- lgd-mybatis-plus/src/main/java/dog/egg/li/mybatisplus/MybatisPlusApplication.java
+++ lgd-mybatis-plus/src/main/java/dog/egg/li/mybatisplus/MybatisPlusApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("dog.egg.li.mybatisplus")
+@MapperScan("dog.egg.li.mybatisplus.dao")
 @SpringBootApplication
 public class MybatisPlusApplication {
 
